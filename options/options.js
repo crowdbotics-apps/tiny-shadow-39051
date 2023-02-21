9d9cb8ebdf6f44d59ff55e20f7b779fcd527b8c0
Crowdbotics <team@crowdbotics.com>
2023-02-21 19:55:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tiny-shadow-39051",
+  url: "https://tiny-shadow-39051.botics.co",
+  api: "https://tiny-shadow-39051.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
